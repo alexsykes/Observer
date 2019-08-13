75a6ed1212de9037762eb20a616f604ef88206c1
Alex Sykes <alex@alexsykes.net>
2019-08-13 10:12:16 +0100

    Added PHP scripts

--- app/src/main/java/com/alexsykes/scoremonster/SyncActivity.java
+++ app/src/main/java/com/alexsykes/scoremonster/SyncActivity.java
@@ -2,6 +2,7 @@ package com.alexsykes.scoremonster;
 
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -33,6 +34,7 @@ import java.util.HashMap;
 
 public class SyncActivity extends AppCompatActivity {
     public static final int TEXT_REQUEST = 1;
+
     /**********  File Path *************/
     final String uploadFilePath = "mnt/sdcard/Documents/Scoremonster/";
     final String uploadFileName = "scores.csv";
@@ -41,7 +43,7 @@ public class SyncActivity extends AppCompatActivity {
     ArrayList<HashMap<String, String>> theScoreList;
     TextView messageText;
     Button uploadButton, processButton;
-    int serverResponseCode = 0;
+    int serverResponseCode = 0, section, trialid;
     ProgressDialog dialog = null;
     String upLoadServerUri = null;
     String processURL = null;
@@ -50,11 +52,19 @@ public class SyncActivity extends AppCompatActivity {
     private ScoreDbHelper mDbHelper;
     private String filename;
 
+    SharedPreferences localPrefs;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync);
+
+        // Get shared preferences for trialid, section
+        localPrefs = getSharedPreferences("monster", MODE_PRIVATE);
+
+        section = localPrefs.getInt("section", 0);
+        trialid = localPrefs.getInt("trialid", 0);
 
         // Create database connection
         mDbHelper = new ScoreDbHelper(this);
@@ -63,7 +73,7 @@ public class SyncActivity extends AppCompatActivity {
         uploadButton = findViewById(R.id.uploadButton);
         processButton = findViewById(R.id.processButton);
 
-        /************* Php script path ****************/
+        /*  Php script path  */
         upLoadServerUri = "http://www.trialmonster.uk/android/UploadToServer.php";
         processURL = "http://www.trialmonster.uk/android/addCSVtodb.php";
 
@@ -174,7 +184,7 @@ public class SyncActivity extends AppCompatActivity {
     }
 
     private void populateScoreList() {
-        theScoreList = mDbHelper.getScoreList();
+        theScoreList = mDbHelper.getScoreList(trialid, section);
         scoreView = findViewById(R.id.scoreView);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         scoreView.setLayoutManager(llm);
@@ -189,7 +199,7 @@ public class SyncActivity extends AppCompatActivity {
     }
 
     public boolean saveToCSV() {
-        String id, observer, section, rider, lap, created, updated, edited, trialid, sync, score;
+        String id, observer, section, rider, lap, created, updated, edited, sync, score, thetrialid;
 
         // Get timestamp and add to filename
 
@@ -214,7 +224,7 @@ public class SyncActivity extends AppCompatActivity {
 
             // Get current data
 
-            Cursor curChild = mDbHelper.getUnSynced();
+            Cursor curChild = mDbHelper.getUnSynced(trialid);
             while (curChild.moveToNext()) {
                 id = curChild.getString(0);
                 observer = curChild.getString(1);
@@ -224,74 +234,11 @@ public class SyncActivity extends AppCompatActivity {
                 created = curChild.getString(5);
                 updated = curChild.getString(6);
                 edited = curChild.getString(7);
-                trialid = curChild.getString(8);
+                thetrialid = curChild.getString(8);
                 sync = curChild.getString(9);
                 score = curChild.getString(10);
 
-                String[] arrStr = {id, section, rider, lap, score, observer, created, updated, edited, trialid, sync
-                };
-
-                csvWrite.writeNext(arrStr, false);
-            }
-            csvWrite.close();
-            return true;
-
-        } catch (IOException e) {
-            Log.e("Child", e.getMessage(), e);
-
-
-            return false;
-        }
-    }
-
-    public boolean oldSaveToCSV() {
-        String id, observer, section, rider, lap, created, updated, edited, trialid, sync, score;
-
-        // Check for existing directory
-        if (!exportDir.exists()) {
-            exportDir.mkdirs();
-        }
-
-        // Get timestamp and add to filename
-
-        Date date = new Date();
-        //getTime() returns current time in milliseconds
-        long time = date.getTime();
-        filename = "scores.csv";
-
-        datafile = new File(exportDir, filename);
-        try {
-
-            datafile.createNewFile();
-            CSVWriter csvWrite = new CSVWriter(new FileWriter(datafile));
-
-            // Output header data in case Cursor is empty
-            //  String headertext = "_id,Rider,Score,Lap";
-
-            //String[] header = {headertext};
-            String[] header = {"id", "section", "rider",
-                    "lap", "score", "observer", "created", "updated", "edited", "trialid",
-                    "sync"};
-            csvWrite.writeNext(header, false);
-
-            // Get current data
-
-            // Cursor curChild = db.rawQuery("SELECT  * FROM scores WHERE sync = " + NOT_SYNCED, null);
-            Cursor curChild = mDbHelper.getUnSynced();
-            while (curChild.moveToNext()) {
-                id = curChild.getString(0);
-                observer = curChild.getString(1);
-                section = curChild.getString(2);
-                rider = curChild.getString(3);
-                lap = curChild.getString(4);
-                created = curChild.getString(5);
-                updated = curChild.getString(6);
-                edited = curChild.getString(7);
-                trialid = curChild.getString(8);
-                sync = curChild.getString(9);
-                score = curChild.getString(10);
-
-                String[] arrStr = {id, section, rider, lap, score, observer, created, updated, edited, trialid, sync
+                String[] arrStr = {id, section, rider, lap, score, observer, created, updated, edited, thetrialid, sync
                 };
 
                 csvWrite.writeNext(arrStr, false);
@@ -334,7 +281,7 @@ public class SyncActivity extends AppCompatActivity {
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
                 dialog.dismiss();
-                mDbHelper.markAsDone();
+                mDbHelper.markAsDone(trialid);
                 populateScoreList();
             }
 
@@ -361,142 +308,6 @@ public class SyncActivity extends AppCompatActivity {
         processCSV.execute();
     }
 
-    public int oldUploadFile(String sourceFileUri) {
-
-
-        String fileName = sourceFileUri;
-
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-        File sourceFile = new File(sourceFileUri);
-
-        if (!sourceFile.isFile()) {
-
-            dialog.dismiss();
-
-            Log.e("uploadFile", "Source File not exist :"
-                    + uploadFilePath + "" + uploadFileName);
-
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    messageText.setText("Source File not exist :"
-                            + uploadFilePath + "" + uploadFileName);
-                }
-            });
-
-            return 0;
-
-        } else {
-            try {
-                // open a URL connection to the Servlet
-                FileInputStream fileInputStream = new FileInputStream(sourceFile);
-                URL url = new URL(upLoadServerUri);
-
-                // Open a HTTP  connection to  the URL
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setDoInput(true); // Allow Inputs
-                conn.setDoOutput(true); // Allow Outputs
-                conn.setUseCaches(false); // Don't use a Cached Copy
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Connection", "Keep-Alive");
-                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-                conn.setRequestProperty("uploaded_file", fileName);
-
-                dos = new DataOutputStream(conn.getOutputStream());
-
-                dos.writeBytes(twoHyphens + boundary + lineEnd);
-                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
-                        + fileName + "\"" + lineEnd);
-
-                dos.writeBytes(lineEnd);
-
-                // create a buffer of  maximum size
-                bytesAvailable = fileInputStream.available();
-
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                buffer = new byte[bufferSize];
-
-                // read file and write it into form...
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                while (bytesRead > 0) {
-
-                    dos.write(buffer, 0, bufferSize);
-                    bytesAvailable = fileInputStream.available();
-                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                }
-
-                // send multipart form data necesssary after file data...
-                dos.writeBytes(lineEnd);
-                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-                // Responses from the server (code and message)
-                serverResponseCode = conn.getResponseCode();
-                String serverResponseMessage = conn.getResponseMessage();
-
-                Log.i("uploadFile", "HTTP Response is : "
-                        + serverResponseMessage + ": " + serverResponseCode);
-
-                if (serverResponseCode == 200) {
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-
-
-                            Toast.makeText(SyncActivity.this, "Score Upload Complete",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-
-                //close the streams //
-                fileInputStream.close();
-                dos.flush();
-                dos.close();
-
-            } catch (MalformedURLException ex) {
-
-                dialog.dismiss();
-                ex.printStackTrace();
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        messageText.setText("MalformedURLException Exception : check script url.");
-                        Toast.makeText(SyncActivity.this, "MalformedURLException",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
-            } catch (Exception e) {
-
-                dialog.dismiss();
-                e.printStackTrace();
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        messageText.setText("Got Exception : see logcat ");
-                        Toast.makeText(SyncActivity.this, "Got Exception : see logcat ",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-                Log.e("Upload file Exception", "Exception : "
-                        + e.getMessage(), e);
-            }
-            dialog.dismiss();
-            return serverResponseCode;
-        }
-    }
-
     public int uploadFile(String sourceFileUri) {
         File directory = getFilesDir();
         File sourceFile = new File(directory, filename);
@@ -511,7 +322,7 @@ public class SyncActivity extends AppCompatActivity {
         String boundary = "*****";
         int bytesRead, bytesAvailable, bufferSize;
         byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
+        int maxBufferSize = 1024 * 1024;
         //File sourceFile = new File(sourceFileUri);
 
         if (!sourceFile.isFile()) {
