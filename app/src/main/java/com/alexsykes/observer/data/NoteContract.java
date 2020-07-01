ad01fafd065794428ea7e65849f9ac4b0fe29e8c
Alex Sykes <alex@alexsykes.net>
2020-07-01 17:02:00 +0100

    Restyle

---
+++ app/src/main/java/com/alexsykes/observer/data/NoteContract.java
@@ -0,0 +1,19 @@
+package com.alexsykes.observer.data;
+
+import android.provider.BaseColumns;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoteContract extends AppCompatActivity {
+
+    private NoteContract() {}
+
+    public static final class NoteEntry implements BaseColumns {
+        public final static String TABLE_NAME = "notes";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_NOTE_OBSERVER = "observer";
+        public final static String COLUMN_NOTE_NOTE = "note";
+        public final static String COLUMN_NOTE_CREATED = "created";
+        public final static String COLUMN_NOTE_SECTION = "section";
+    }
+}
