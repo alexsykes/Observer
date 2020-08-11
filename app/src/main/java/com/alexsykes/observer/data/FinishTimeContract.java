88ed0a6cb83fe8d1247ddd0d30c1003e0c384e4c
Alex Sykes <alex@alexsykes.net>
2020-08-11 16:17:18 +0100

    WiP - needs debugging

--- app/src/main/java/com/alexsykes/observer/data/FinishTimeContract.java
+++ app/src/main/java/com/alexsykes/observer/data/FinishTimeContract.java
@@ -16,5 +16,6 @@ public class FinishTimeContract extends AppCompatActivity {
         public final static String COLUMN_FINISHTIME_RIDE_TIME = "ridetime";
         public final static String COLUMN_FINISHTIME_SYNC = "sync";
         public final static String COLUMN_FINISHTIME_HUMANREADABLE = "finishtime_humanreadable";
+        public final static String COLUMN_FINISHTIME_TRIALID = "trialid";
     }
 }
