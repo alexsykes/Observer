869d418fef2396d040a0fd3d100efe41449d857a
Alex Sykes <alex@alexsykes.net>
2021-06-14 11:39:40 +0100

    Start

---
+++ app/src/main/java/com/alexsykes/observer/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.alexsykes.observer.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alexsykes.observer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
